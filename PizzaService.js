6c21a6c7f42962e51ee12b7b880430eca032eca5
Cekiso <nonkululeko@quirky30.co.za>
2021-09-14 15:18:57 +0200

    all

--- PizzaService.js
+++ PizzaService.js
@@ -1,9 +1,64 @@
-module.exports = function(pizzaList) {
+module.exports = function PizzaApp() {
 
-    const pizzas = pizzaList || [];
+    let smallCost;
+    let mediumTotal;
+    let largeTotal;
 
-    function listAll() {
+
+    var smallTotalPrice = 0;
+    var mediumTotalPrice = 0;
+    var largeTotalPrice = 0;
+    // const pizzas = pizzaList || [];
+    var pizzas = {};
+    var message = "";
+
+    function listAll(size) {
+        var sizes = pizzas;
+        // console.log(sizes)
+        setList(size)
+        if (size == 'small' && price == 'number') {
+            message = small;
+        } else if (size == 'medium') {
+            message = medium;
+        } else if (size == 'large') {
+            message = large;
+        } else {
+            message = '';
+        }
+        // var size = pizzas;
+        // var count = size.rows;
+        // for (var i = 0; i < count; i++) {
+        //     console.log(size.rows[i]);
+        //}
+
+
+    }
+
+    function setList(size) {
+        smallCost += smallTotalPrice;
+
+        if (pizzas[size] === undefined) {
+            pizzas[size] = 1
+        } else {
+            pizzas[size]++
+        }
+
+    }
+
+    function getLists() {
+        return message
+    }
+
+    function count() {
+        var pizzaList = Object.keys(pizzas);
+        console.log(pizzaList)
+        return pizzaList.length;
+    }
+    //create a function that returns all the name in the list 
+
+    function sizeList() {
         return pizzas;
+
     }
 
     function addPizza(pizza) {
@@ -23,6 +78,10 @@ module.exports = function(pizzaList) {
         deletePizza,
         updatePizza,
         listAll,
+        setList,
+        getLists,
+        sizeList,
+        count,
     }
 
 }
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-var session = require('express-session')
+var session = require('express-session');
+const PizzaApp = require('./PizzaService');
+const pizza = PizzaApp();
 
 
 const app = express();
@@ -38,13 +40,43 @@ let counter = 0;
 app.get('/', function(req, res) {
     res.render('index', {
         counter
+
     });
+
 });
 
 app.post('/count', function(req, res) {
     counter++;
+
     res.redirect('/')
 });
+
+app.get('/Buy', function(req, res) {
+    res.render('index', {
+        counter,
+        list: pizza.listAll()
+    })
+
+})
+app.post('/Buy', function(req, res) {
+
+    // console.log(pizza.listAll(req.body.size))
+    pizza.listAll(req.body.size)
+
+    res.redirect('/');
+})
+
+app.get('/order', function(req, res) {
+    res.render('index', {
+
+    })
+
+})
+app.post('/order', function(req, res) {
+
+
+    res.render('index');
+})
 
 app.get('/info', function(req, res) {
     res.render('names', {
